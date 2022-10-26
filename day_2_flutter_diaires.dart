2351b9dd35dc354bf4e12e8a2f4620f13b8fb228
Beatriz Rios <110923126+beatrizriost@users.noreply.github.com>
2022-10-26 15:29:02 -0300

    Create day_2_flutter_diaires.dart

---
+++ day_2_flutter_diaires.dart
@@ -0,0 +1,24 @@
+// 1 - Introdução a variáveis 
+  
+  String variavelNome = "Beatriz";
+  print (variavelNome);
+  // o valor de uma string deve ser inserido entre aspas duplas ""
+  int valorVariavel = 10;
+  print (valorVariavel);
+  // int = numeros inteiros
+  bool ehVerdadeiro = true;
+  print (ehVerdadeiro);
+  // bool = booleano (v ou f)
+  List<String> listaDePalavras = ["Beatriz", "Rios"];
+  print ('${listaDePalavras[0]}-${listaDePalavras[1]}');
+  
+  // 2- introducão null-safety
+  
+  String? nome;
+  print (nome!);
+  
+  late String sobrenome;
+  sobrenome = "Rios";
+  print (sobrenome);
+ 
+}
