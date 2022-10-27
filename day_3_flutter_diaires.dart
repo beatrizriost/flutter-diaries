0af8e944e361d34cdc209826e76b1452ac8cc90e
Beatriz Rios <110923126+beatrizriost@users.noreply.github.com>
2022-10-27 14:39:43 -0300

    Create day_3_flutter_diaires.dart

---
+++ day_3_flutter_diaires.dart
@@ -0,0 +1,29 @@
+void main() {
+  
+// 3 - IF e SWITCH
+
+bool seguirEmFrente = false;
+  if(seguirEmFrente) { 
+  print("Andar"); 
+  } else { 
+    print ("Parar");}
+  
+  if (10 > 5) {
+    print ("10 é maior que 5");
+  }
+ 
+  int valorInt = 5;
+  switch(valorInt){
+    case 0: 
+      print ("zero");
+      break;
+     case 1: 
+      print ("um");
+      break;
+     case 2: 
+      print ("dois");
+      break;
+     default :
+      print ("padrão");
+  }
+}
